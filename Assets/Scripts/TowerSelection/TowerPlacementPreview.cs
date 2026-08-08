c667e9fb091135de7dce304dab8bc10ba1bd7432
DimaTkachenko340297 <zvdmitry2006@gmail.com>
2026-08-08 17:42:16 +0200

    feat: add tower placement preview logic
    - Add TowerPlacementPreview script with placement validation state
    - Implement dynamic color and transparency updates based on valid/invalid placement

---
+++ Assets/Scripts/TowerSelection/TowerPlacementPreview.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class TowerPlacementPreview : MonoBehaviour
+{ 
+    [SerializeField] private Color _validPlacementColor;
+    [SerializeField] private Color _placementBlockedColor;
+    [SerializeField] [Range(0f, 1f)] private float _previewAlpha;
+
+    private SpriteRenderer _spriteRenderer;
+    private bool _isValid;
+    public bool IsValid
+    {
+        get => _isValid;
+        set
+        {
+            _isValid = value;
+            ApplyColor();
+        }
+    }
+
+
+    private void Awake() 
+    {
+        ApplyColor();
+    }
+
+    private void ApplyColor()
+    {
+        if (_spriteRenderer == null)
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Color targetColor = _isValid ? _validPlacementColor : _placementBlockedColor;
+        targetColor.a = _previewAlpha;
+        _spriteRenderer.color = targetColor;
+    }
+}
